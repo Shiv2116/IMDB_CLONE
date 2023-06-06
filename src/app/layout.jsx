f26cf26ba5540d7d1a8d50a1f427af75f5bb4be9
Shiv2116 <114937365+Shiv2116@users.noreply.github.com>
2023-06-06 22:08:25 +0530

    added navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import './globals.css'
 import Providers from './Providers'
+import Navbar from '@/components/Navbar'
 
 
 export const metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <Providers>
         <Header/>
         {/* {navbar} */}
+        <Navbar/>
         {/* {searchbox} */}
         {children}
         </Providers>
